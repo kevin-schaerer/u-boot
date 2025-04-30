df36ce607797d8627f792a06a1abca2d699af0aa
Kevin Schaerer <kevin.schaerer@nettimelogic.com>
2025-04-30 17:59:05 +0200

    add preliminary Hive-S

---
+++ board/ntl/hive-s/ntl-hive-s.c
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (c) 2019 SiFive, Inc
+ * Copyright (c) 2025 Kevin Schaerer <kevin.schaerer@nettimelogic.com>
+ */
+#include <init.h>
+#include <configs/ntl-hive-s.h>
+#include <dm.h>
+#include <spl.h>
+
+#ifdef CONFIG_SPL
+void board_boot_order(u32 *spl_boot_list)
+{
+	u8 i;
+	u32 boot_devices[] = {
+		BOOT_DEVICE_MMC1,
+	};
+
+	for (i = 0; i < ARRAY_SIZE(boot_devices); i++)
+		spl_boot_list[i] = boot_devices[i];
+}
+#endif
+
+int board_init(void)
+{
+		return 0;
+}
---
+++ include/configs/ntl-hive-s.h
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (c) 2019 Western Digital Corporation or its affiliates.
+ * Copyright (c) 2025 Kevin Schaerer <kevin.schaerer@nettimelogic.com>
+ */
+
+#ifndef __NTL_HIVE_S_CONFIG_H
+#define __NTL_HIVE_S_CONFIG_H
+
+#include <linux/sizes.h>
+
+/* Environment options */
+#define CFG_SYS_SDRAM_BASE 0x40000000
+
+/* ---------------------------------------------------------------------
+ * Board boot configuration
+ */
+
+#define CFG_EXTRA_ENV_SETTINGS \
+	"fdt_addr_r=0x40ef0000\0" \
+	"kernel_addr_r=0x40000000\0" \
+	"image=boot/Image\0" \
+	"mmcdev=0\0" \
+	"mmcpart=1\0"
+
+#endif/* __CONFIG_H */
